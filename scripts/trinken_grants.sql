23e3cd4d88beeb0f438c84b4f2a94a0ada1dea49
sebas moran <sebasmorandez@hotmail.com>
2022-05-03 12:56:22 -0500

    created grant script to grant all tables privilege

---
+++ scripts/trinken_grants.sql
@@ -0,0 +1,23 @@
+grant all privileges on tk_articulos to MX_A160_PLSQL_S05;
+grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S05;
+grant all privileges on tk_clientes to MX_A160_PLSQL_S05;
+grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S05;
+grant all privileges on tk_pedidos to MX_A160_PLSQL_S05;
+grant all privileges on tk_provedoores to MX_A160_PLSQL_S05;
+grant all privileges on tk_repartidores to MX_A160_PLSQL_S05;
+
+grant all privileges on tk_articulos to MX_A160_PLSQL_S09;
+grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S09;
+grant all privileges on tk_clientes to MX_A160_PLSQL_S09;
+grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S09;
+grant all privileges on tk_pedidos to MX_A160_PLSQL_S09;
+grant all privileges on tk_provedoores to MX_A160_PLSQL_S09;
+grant all privileges on tk_repartidores to MX_A160_PLSQL_S09;
+
+grant all privileges on tk_articulos to MX_A160_PLSQL_S08;
+grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S08;
+grant all privileges on tk_clientes to MX_A160_PLSQL_S08;
+grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S08;
+grant all privileges on tk_pedidos to MX_A160_PLSQL_S08;
+grant all privileges on tk_provedoores to MX_A160_PLSQL_S08;
+grant all privileges on tk_repartidores to MX_A160_PLSQL_S08;
