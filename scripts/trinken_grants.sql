59296d577c9b350e7cbeaad9b2c83fef1582b315
sebas moran <sebasmorandez@hotmail.com>
2022-05-03 13:33:19 -0500

    fixed typo in proveedores

--- scripts/trinken_grants.sql
+++ scripts/trinken_grants.sql
@@ -3,7 +3,7 @@ grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S05;
 grant all privileges on tk_clientes to MX_A160_PLSQL_S05;
 grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S05;
 grant all privileges on tk_pedidos to MX_A160_PLSQL_S05;
-grant all privileges on tk_provedoores to MX_A160_PLSQL_S05;
+grant all privileges on tk_proveedores to MX_A160_PLSQL_S05;
 grant all privileges on tk_repartidores to MX_A160_PLSQL_S05;
 
 grant all privileges on tk_articulos to MX_A160_PLSQL_S09;
@@ -11,7 +11,7 @@ grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S09;
 grant all privileges on tk_clientes to MX_A160_PLSQL_S09;
 grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S09;
 grant all privileges on tk_pedidos to MX_A160_PLSQL_S09;
-grant all privileges on tk_provedoores to MX_A160_PLSQL_S09;
+grant all privileges on tk_proveedores to MX_A160_PLSQL_S09;
 grant all privileges on tk_repartidores to MX_A160_PLSQL_S09;
 
 grant all privileges on tk_articulos to MX_A160_PLSQL_S08;
@@ -19,5 +19,5 @@ grant all privileges on tk_ato_pedidos to MX_A160_PLSQL_S08;
 grant all privileges on tk_clientes to MX_A160_PLSQL_S08;
 grant all privileges on tk_drn_clientes to MX_A160_PLSQL_S08;
 grant all privileges on tk_pedidos to MX_A160_PLSQL_S08;
-grant all privileges on tk_provedoores to MX_A160_PLSQL_S08;
+grant all privileges on tk_proveedores to MX_A160_PLSQL_S08;
 grant all privileges on tk_repartidores to MX_A160_PLSQL_S08;
